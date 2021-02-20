a6c8c06c164f98e8e94b4e565ef3bfbb69f74d01
Victor Shen <victorshen4444@gmail.com>
2021-02-20 15:21:38 -0800

    improved censor

--- src/controllers/censor.js
+++ src/controllers/censor.js
@@ -1,12 +1,21 @@
 import fuzzysort from 'fuzzysort';
 import Filter from 'bad-words';
 
-// let message = 'the fucking president is doanaald mctrumpy you b1tch suck my c0ck!! i love whtie supremacy';
+// let message = 'the fucking president is doald mctrumpy you b1tch suck my c0ck!! i love whtie supremacy';
 // const banned = ["donald","trump","white"];
 
 export const censor = (message, banned) => {
   const replace = [];
 
+
+  const origSize = banned.length;
+  for(let i = 0;i<origSize;i++){
+    const word = banned[i];
+    for(let j = 0;j<word.length;j++){
+      banned.push(word.substring(0, j) + word.substring(j+1));
+    }
+  }
+  
   const words = message.split(' ');
   for(const word of words){
     for (const ban of banned){
@@ -15,7 +24,6 @@ export const censor = (message, banned) => {
       });
       if(results){
         replace.push(word);
-        replace.push(ban);
       }
     } 
   }
@@ -25,6 +33,10 @@ export const censor = (message, banned) => {
   message = filter.clean(message);
   return message;
 }
+
+// console.log(banned);
+// console.log(message);
+// console.log(censor(message, banned));
 
 
 
