a9ee05902bb1fb607bdfd556b7aff7f3805574b0
Kenzie Tee <teekenzie@berkeley.edu>
2021-02-20 11:01:44 -0800

    wrote out fuzzy prompt:

--- tryOut.js
+++ tryOut.js
@@ -1,11 +1,19 @@
 import fuzzy from 'fuzzy';
 import FuzzySearch from 'fuzzy-search';
 
-let message = 'My name is XYkZ';
+/*
+Using fuzzy or fuzzy search, write the code where given a message, asterick out the banned wrods
+
+banned = ["XYZ", "name"]
+message = "My nama is XYZ"
+result = My **** is ***
+*/
+
+let message = 'My name is XYZ';
 var list = message.split(' ');
 console.log(list);
 
-var results = fuzzy.filter('XYZ', list);
+var results = fuzzy.filter('XYkZ', list);
 var matches = results.map((el) => el.string);
 
 console.log('ran');
