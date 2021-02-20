c337b17c953b9f26784bd9770248ee575a703fc9
Kenzie Tee <teekenzie@berkeley.edu>
2021-02-19 23:06:17 -0800

    uninstalled express-validator

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { register } from '../controllers/auth.js';
-import { check } from 'express-validator';
 
 const router = Router();
 router.route('/').post(register);
