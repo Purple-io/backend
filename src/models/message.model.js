5876e853332b7b5cfded42fc971cdffdd7c62ab8
Kenzie Tee <teekenzie@berkeley.edu>
2021-02-21 00:18:43 -0800

    created test route

--- src/models/message.model.js
+++ src/models/message.model.js
@@ -1,23 +1,25 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
-const messageSchema = new mongoose.Schema({
-  messageId: { type: mongoose.Schema.Types.ObjectId },
-  fromUser: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
+const messageSchema = new mongoose.Schema(
+  {
+    messageId: { type: mongoose.Schema.Types.ObjectId },
+    fromUser: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    toUser: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
   },
-  toUser: {
-    type: Schema.Types.ObjectId,
-    ref: 'user',
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: { currentTime: () => new Date() } }
+);
 
-messageSchema.set('timestamps', true);
 export default mongoose.model('message', messageSchema);
